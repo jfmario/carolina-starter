a4e5a6690c7be0523f9feaf2f7acdbd033112122
John F Marion <john.f.marion@gmail.com>
2019-05-02 17:49:41 +0000

    clean test command

--- site/terminal/commands/test-command.js
+++ site/terminal/commands/test-command.js
@@ -23,16 +23,7 @@ class TestCommand extends BaseCommand {
 
   async handle(args) {
     
-    let S3FileDriver = require('carolina/plugins/aws/plugin/files/file-drivers')['Carolina/AWS/S3FileDriver'];
-    
-    let driver = new S3FileDriver();
-    driver.configure({ profile: 'default', bucketName: 'johnfmarion' });
-    
-    // let results1 = await driver.writeFile("trainpavach/text/Homework06.txt", "ok");
-    // console.log(results1);
-    
-    let results2 = await driver.deleteFile("test.txt");
-    console.log(results2);
+    console.log("This is a test command.");
   }
 }
 
